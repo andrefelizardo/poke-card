549acd6cdbf2f4c81dd256f07a55cdea44bb1886
André Felizardo <andre.luis.felizardo@accenture.com>
2020-03-18 17:35:16 -0300

    Encapsulando o componente.

--- pokeCard.js
+++ pokeCard.js
@@ -1,8 +1,24 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <style>
+        h3 {
+            color: coral;
+        }
+    </style>
+
+    <h3></h3>
+`;
+
 class PokeCard extends HTMLElement {
     constructor() {
         super();
+
+        this.attachShadow({ mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
         const name = this.getAttribute('name'); 
-        this.innerHTML = `${name}`;
+        this.shadowRoot.querySelector('h3').innerText = name;
     }
 }
 
