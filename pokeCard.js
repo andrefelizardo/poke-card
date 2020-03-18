c200838698129da7bc0d05a4118d5af708217ab5
André Felizardo <andre.luis.felizardo@accenture.com>
2020-03-18 17:46:16 -0300

    Adicionando mais um card e imagem.

--- pokeCard.js
+++ pokeCard.js
@@ -2,24 +2,31 @@ const template = document.createElement('template');
 
 template.innerHTML = `
     <style>
+        img {
+            width: 150px;
+        }    
+    
         h3 {
-            color: coral;
+                color: coral;
         }
     </style>
 
+    <img />
     <h3></h3>
 `;
 
 class PokeCard extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.attachShadow({ mode: 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-        const name = this.getAttribute('name'); 
-        this.shadowRoot.querySelector('h3').innerText = name;
-    }
+    const name = this.getAttribute('name');
+    const imageURL = this.getAttribute('avatar');
+    this.shadowRoot.querySelector('h3').innerText = name;
+    this.shadowRoot.querySelector('img').setAttribute('src', imageURL);
+  }
 }
 
 window.customElements.define('poke-card', PokeCard);
